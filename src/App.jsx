37d83dddbbed84a168d13e661e5f9174365ec0ce
Yahia Salah <109294230+YahiaSala7@users.noreply.github.com>
2023-07-14 06:16:40 +0300

    test 4

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,7 @@ function App() {
     <>
       <Header />
       <Landing />
-      {/* this is a comment from noor  */}
-
-      {/* this is a comment from noor  */}
-      {/* this is a comment from noor  */}
-
-      {/* hey im here  */}
-      {/* hey im here  */}
-      {/* hey im here  */}
-      {/* hey im here  */}
-      {/* hey im here  */}
-      {/* hey im here  */}
-      {/* hey im here again */}
-      {/* hey im here test3 */}
-      {/* hey im here newbranch */}
+      {/* the final test */}
     </>
   );
 }
