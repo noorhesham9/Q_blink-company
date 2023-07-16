ac56b4cafefe495eb5ba630925494eef708fbba0
Yahia Salah <109294230+YahiaSala7@users.noreply.github.com>
2023-07-16 06:52:31 +0300

    some modifying

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@emotion/react";
 import "./App.css";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
@@ -5,11 +6,25 @@ import Footer from "./components/Footer";
 import Header from "./components/Header/Header";
 import Works from "./components/Works/Works";
 import Landing from "./components/landing/Landing";
+import { Box } from "@mui/material";
+import { useState } from "react";
 // import Testimonials from "./components/testimonials/Testimonials";
 // import { Stack } from "@mui/material";
 function App() {
+  const [theme, setTheme] = useState("light-theme");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+  // const [mode, setMode] = useState("light");
+  // const modeTheme = createTheme({
+  //   palette: {
+  //     mode: mode,
+  //   },
+  // });
+
   return (
-    <>
+    <Box className={theme}>
       <Header />
       <Landing />
       <About />
@@ -17,7 +32,7 @@ function App() {
       <Works />
       <Contact />
       <Footer />
-    </>
+    </Box>
   );
 }
 
