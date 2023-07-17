c936a95d38c2b0fe3c15de8f76b24d743b3a7ab2
Yahia Salah <109294230+YahiaSala7@users.noreply.github.com>
2023-07-17 04:40:15 +0300

    add emailjs

--- src/components/Contact/FormikContainer.jsx
+++ src/components/Contact/FormikContainer.jsx
@@ -2,6 +2,7 @@ import { Form, Formik } from "formik";
 import * as Yup from "yup";
 import FormikControl from "./FormikControl";
 import { Button, Stack } from "@mui/material";
+import emailjs from "@emailjs/browser";
 import "./form.css";
 const initialValues = {
   name: "",
@@ -16,7 +17,32 @@ const validationSchema = Yup.object({
   message: Yup.string().required("Required !"),
 });
 function FormikContainer() {
-  const handleSubmit = () => console.log("");
+  const handleSubmit = (values) => {
+    const { name, email, message, subject } = values;
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+
+        {
+          from_name: name,
+          to_name: "Q-Blink Company",
+          from_email: email,
+          to_email: "qblink02@gmail.com",
+          message: `the subject is ${subject} and the messasge is ${message}`,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          alert("Thank you. I will get back to you as soon as possible.");
+        },
+        (error) => {
+          console.error(error);
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
 
   return (
     <Formik
